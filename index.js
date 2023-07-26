4c45bf977102c616261715088a70ab160f5dcb35
Kanny <kanny.ghafour87@gmail.com>
2023-07-26 16:29:03 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+      name: userName,
+      email: userEmail
+    };
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", fetchOptions)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML=data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
